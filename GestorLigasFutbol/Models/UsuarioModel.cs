5271091514739660bdc59af9a9535d4905f1bbad
profGeisel <163931659+profGeisel@users.noreply.github.com>
2025-04-18 23:02:42 -0600

    Usuarios
    Elementos para el registro de usuarios a la BD, procedimiento para mostrar los registros de usuarios existentes en tabla.

---
+++ GestorLigasFutbol/Models/UsuarioModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace GestorLigasFutbol.Models
+{
+    public class UsuarioModel
+    {
+        public int Id { get; set; }
+
+        [Required (ErrorMessage="El nombre es requerido")]
+        [StringLength(20, ErrorMessage="Maximo 20 caracteres")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El Apellido Paterno es requerido")]
+        [StringLength(20, ErrorMessage = "Maximo 20 caracteres")]
+        public string ApellidoP { get; set; }
+
+        [Required(ErrorMessage = "El Apellido Materno es requerido")]
+        [StringLength(20, ErrorMessage = "Maximo 320 caracteres")]
+        public string ApellidoM { get; set; }
+
+
+        [Required(ErrorMessage = "El correo Electronico es requerido")]
+        public string CorreoE { get; set; }
+
+        [Required(ErrorMessage = "La contraseña es requerida")]
+        public string Contrasena { get; set; }
+
+        [Required(ErrorMessage = "El tipo de Usuario es requerida")]
+        public int IdTipoUsuario { get; set; }
+    }
+}
